ce9c7a5a96ed2c7517374d6458213e4699094226
Pierce Gleeson <piercegleeson@dhcp-10-105-110-241.wireless.northwestern.private>
2016-02-29 18:10:11 -0600

    consolidated files

---
+++ toolbar.js
@@ -0,0 +1,16 @@
+function search(){
+	var query = document.getElementById("searchtxt");
+	var querytxt = query.value;
+	if (querytxt == '60201' || querytxt == 'Evanston'){
+		window.location.href = 'searchresult.html';
+		alert("correct input");
+	}
+	else{
+		window.location.href = 'noresult.html';
+		alert(querytxt);
+	}
+}
+
+function gohome(){
+	window.location.href = 'homepage.html';
+}
